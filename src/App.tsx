d87eecca39bc1d08c62ea7678616d7bb8cdb7b84
dev-seomoon <skm1104@daum.net>
2022-03-16 16:47:59 +0900

    Feature: Search UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,26 @@
+import { useState } from 'react';
+import Search from './components/Search';
+
+type UserType = {
+  id: string;
+  name: string;
+  login: string;
+  imgUrl: string; // avatar_url
+  contact: string; // bio
+};
+
 export default function App() {
-  return <div className="text-lg">Hello, World!</div>;
+  const [user, setUser] = useState<UserType | null>({
+    id: '52448114',
+    name: 'seomoon',
+    login: 'dev-seomoon',
+    imgUrl: 'https://avatars.githubusercontent.com/u/52448114?v=4',
+    contact: 'seomoon.frontend@gmail.com',
+  });
+
+  return (
+    <div className="relative text-lg">
+      <Search user={user} />
+    </div>
+  );
 }
---
+++ src/components/Search.tsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react';
+
+type UserType = {
+  id: string;
+  name: string;
+  login: string;
+  imgUrl: string; // avatar_url
+  contact: string; // bio
+};
+
+type UserProps = {
+  user: UserType | null;
+};
+
+function Search({ user }: UserProps): React.ReactElement {
+  const [value, setValue] = useState('');
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => setValue(e.target.value);
+
+  const handleClick = () => {
+    // fetchUser();
+  };
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    if (e.key === 'Enter') {
+      // fetchUser();
+    }
+  };
+
+  return (
+    <section className="absolute top-8 left-10 rounded-lg bg-white px-6  pt-2 pb-4 shadow-lg first-letter:space-y-4">
+      <form className="flex items-center space-x-2">
+        <input
+          className="border-b border-gray-200 px-1 py-2 focus:outline-none focus:ring-0 placeholder:text-base"
+          placeholder="Github 아이디 검색"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+        />
+        <button type="button" className="text-gray-500" onClick={handleClick}>
+          ⏎
+        </button>
+      </form>
+      {user && (
+        <>
+          <div className="mt-4 flex items-center space-x-4">
+            <img src={user.imgUrl} alt="github profile" className="h-12 w-12 rounded-2xl" />
+            <div className="text-sm text-gray-600">
+              <p className="space-x-1">
+                <a href={`https://github.com/${user.login}`} className="font-semibold hover:text-black">
+                  {user.login}
+                </a>
+                <span className="ml-1">
+                  (
+                  {user.name}
+                  )
+                </span>
+              </p>
+              {user.contact && <p className="text-ellipsis">{user.contact}</p>}
+            </div>
+          </div>
+          <p className="mt-4 pt-4 text-sm text-gray-400">
+            * 유저들 간의 스타를 누른 리포지토리 관계를 보여줍니다.
+          </p>
+        </>
+      )}
+    </section>
+  );
+}
+
+export default Search;
