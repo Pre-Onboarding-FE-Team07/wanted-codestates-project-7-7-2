8aa4422e62a884cd30e6a8d175a217d28aac1988
Park, Jinyong <21158729+jinyongp@users.noreply.github.com>
2022-03-16 18:34:23 +0900

    Feature: Apollo GraphQL, graphql-codegen 설치 및 설정 (#2)

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
+import { ApolloProvider } from '@apollo/client';
 import ReactDOM from 'react-dom';
 import App from './App';
+import client from './client';
 import './tailwind.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root'),
+);
