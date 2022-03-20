7cdf0e8b0c9f7f0ba0fa02dfa8e4c1ad64f0ecd0
dev-seomoon <skm1104@daum.net>
2022-03-21 08:53:50 +0900

    Init: checkout deploy branch with build directory

--- src/react-app-env.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react-scripts" />
-
-declare namespace NodeJS {
-  interface ProcessEnv {
-    REACT_APP_GITHUB_TOKEN: string;
-  }
-}
--- src/types/object.d.ts
+++
@@ -1,5 +0,0 @@
-import { UserReposQuery } from '../generated/graphql';
-
-export type UserRepoType = NonNullable<UserReposQuery['user']>;
-export type UserType = Omit<UserRepoType, 'starredRepositories'>;
-export type RepoType = NonNullable<NonNullable<Pick<UserRepoType, 'starredRepositories'>['starredRepositories']['nodes']>[0]>;
